d48fbe706074aab9c10dd43f071699f4d131509a
Daniel Kim <dkim848@aucklanduni.ac.nz>
2024-03-26 00:14:48 +1300

    used a try-catch statement for if hireFeeInput is not an integer, also organised code by adding comments and gaps to make it more readable. Made the venue being created in the very last if else statement

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -23,22 +23,32 @@ public class VenueHireSystem {
 
   public void printVenues() {
     // TODO implement this method
-    if (venueNameList.isEmpty()) {
+     if (venueNameList.isEmpty()) {
       MessageCli.NO_VENUES.printMessage();
-    }
+    } 
   }
 
   public void createVenue(String venueName, String venueCode, String capacityInput, String hireFeeInput) {
-    if (Integer.parseInt(capacityInput) < 0) {
+
+    if (Integer.parseInt(capacityInput) < 0) {                                                                          // If capacityInput is less than 0
       MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
-    } else if (!venueName.isEmpty() && !venueCode.isEmpty() && !capacityInput.isEmpty() && !hireFeeInput.isEmpty()) {
+    }
+    else if (hireFeeInput != null) {                                                                                    // If hireFeeInput is not an int number
+      try {
+        Integer.parseInt(hireFeeInput);
+      } catch (Exception e) {
+        MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
+      }
+    }
+    else if (venueName.isEmpty()) {                                                                                     // If the venueName is empty
+      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
+    }
+    else if (!venueName.isEmpty() && !venueCode.isEmpty() && !capacityInput.isEmpty() && !hireFeeInput.isEmpty()) {     // If all the inputs are correct and not empty then create
       venueNameList.add(id, venueName);
       venueCodeList.add(id, venueCode);
       venueCapacityList.add(id, capacityInput);
       venueFeeList.add(id, hireFeeInput);
       MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName, venueCode);
-    } else if (venueName.isEmpty()) {
-      MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
     }
   }
 
